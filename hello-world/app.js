eb2d2ba6cca26c8a0208c427a52784155bc9fdbd
Anto Jonie <antojoniew@gmail.com>
2022-04-12 23:09:37 +0530

    app.js

--- hello-world/app.js
+++ hello-world/app.js
@@ -5,7 +5,7 @@ exports.lambdaHandler = async (event, context) => {
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'helloworld',
+                message: 'hello main',
             })
         }
     } catch (err) {
